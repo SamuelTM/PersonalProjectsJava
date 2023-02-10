d7a5ebd84da471b25c5f46400a2399a29c6ef877
Samuel Medeiros <steixeiramedeiros@gmail.com>
2023-02-10 10:31:23 -0300

    agora sim, tinha esquecido de adicionar esse

--- src/main/java/samueltm/projetospessoais/outros/Aleatoriedade.java
+++ src/main/java/samueltm/projetospessoais/outros/Aleatoriedade.java
@@ -6,35 +6,56 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Aleatoriedade {
 
-    public static <T> List<T> amostraPonderada(ColecaoPonderada<T> colecao, int n) {
-        if (colecao.getNumElementos() < n)
+    public static <T> List<T> amostraPonderada(ColecaoPonderada<T> colecao, int tamanhoAmostra) {
+        if (colecao.getNumElementos() < tamanhoAmostra)
             throw new IllegalArgumentException("Número de elementos menor que amostra desejada");
-        if (n < 1) throw new IllegalArgumentException("Tamanho da amostra deve ser positivo");
+        if (tamanhoAmostra < 1) throw new IllegalArgumentException("Tamanho da amostra deve ser positivo");
 
         List<T> resultado = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < tamanhoAmostra; i++) {
             resultado.add(colecao.proximo());
         }
 
         return resultado;
     }
 
-    public static <T> List<T> amostraSimples(List<T> elementos, int n) {
-        if (elementos.size() < n)
+    public static <T> List<T> amostraSimples(List<T> elementos, int tamanhoAmostra) {
+        if (elementos.size() < tamanhoAmostra)
             throw new IllegalArgumentException("Número de elementos menor que amostra desejada");
-        if (n < 1) throw new IllegalArgumentException("Tamanho da amostra deve ser positivo");
+        if (tamanhoAmostra < 1) throw new IllegalArgumentException("Tamanho da amostra deve ser positivo");
 
         List<T> resultado = new ArrayList<>();
 
         for (int i = 0; i < elementos.size(); i++) {
-            int quantosPrecisamos = n - resultado.size();
-            int quantoAindaTemos = elementos.size() - i;
-            double probabilidade = (double) quantosPrecisamos / quantoAindaTemos;
-            if (ThreadLocalRandom.current().nextDouble() <= probabilidade) {
+            if (resultado.size() < tamanhoAmostra) {
                 resultado.add(elementos.get(i));
+            } else {
+                int indiceAleatorio = ThreadLocalRandom.current().nextInt(i + 1);
+                if (indiceAleatorio < tamanhoAmostra) {
+                    resultado.set(indiceAleatorio, elementos.get(i));
+                }
             }
         }
+        return resultado;
+    }
 
+    public static <T> List<T> amostraSimples(T[] elementos, int tamanhoAmostra) {
+        if (elementos.length < tamanhoAmostra)
+            throw new IllegalArgumentException("Número de elementos menor que amostra desejada");
+        if (tamanhoAmostra < 1) throw new IllegalArgumentException("Tamanho da amostra deve ser positivo");
+
+        List<T> resultado = new ArrayList<>();
+
+        for (int i = 0; i < elementos.length; i++) {
+            if (resultado.size() < tamanhoAmostra) {
+                resultado.add(elementos[i]);
+            } else {
+                int indiceAleatorio = ThreadLocalRandom.current().nextInt(i + 1);
+                if (indiceAleatorio < tamanhoAmostra) {
+                    resultado.set(indiceAleatorio, elementos[i]);
+                }
+            }
+        }
         return resultado;
     }
 
